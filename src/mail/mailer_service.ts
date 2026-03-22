e7f8f7f1df6e624677e13ead80bd4ff0a0f8fd7b
Davi Herbes <hmatosdavi@gmail.com>
2026-03-22 13:02:36 -0300

    tentando usar o sendgrid

--- src/mail/mailer_service.ts
+++ src/mail/mailer_service.ts
@@ -7,12 +7,14 @@ export class MailService {
   async sendUserConfirmation(username: string, email: string, code: string) {
     try {
       console.log(process.env.EMAIL_KEY);
-      await sgMail.send({
+      const response = await sgMail.send({
         from: "d47701206@gmail.com",
         to: email,
         subject: "Confirme seu email",
         html: `<div class="container"><h1>Olá, ${username} o código para autenticação é: </h1><br><h2>${code}</h2></div>`,
       });
+
+      console.log(response);
     } catch (e) {
       console.error("------ERRO DO EMAIL-------", e);
       throw e;
