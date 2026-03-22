f8920dbd8e906d1db63c34d730338823cea6c7e2
Davi Herbes <hmatosdavi@gmail.com>
2026-03-22 11:02:50 -0300

    reconfigurando nodemailer

--- src/mail/mailer_service.ts
+++ src/mail/mailer_service.ts
@@ -1,26 +1,29 @@
 import * as nodemailer from "nodemailer";
 
 export class MailService {
-	private transporter: nodemailer.Transporter;
+  private transporter: nodemailer.Transporter;
 
-	constructor() {
-		this.transporter = nodemailer.createTransport({
-			host: "smtp.gmail.com",
-			port: 587,
-			secure: false,
-			auth: {
-				user: process.env.USER_EMAIL,
-				pass: process.env.PASSWORD_EMAIL,
-			},
-		});
-	}
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: true,
+      auth: {
+        user: process.env.USER_EMAIL,
+        pass: process.env.PASSWORD_EMAIL,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+  }
 
-	async sendUserConfirmation(username: string, email: string, code: string) {
-		await this.transporter.sendMail({
-			from: '"Simple Messenger" d47701206@gmail.com',
-			to: email,
-			subject: "Confirme seu email",
-			html: `<div class="container"><h1>Olá, ${username} o código para autenticação é: </h1><br><h2>${code}</h2></div>`,
-		});
-	}
+  async sendUserConfirmation(username: string, email: string, code: string) {
+    await this.transporter.sendMail({
+      from: '"Simple Messenger" d47701206@gmail.com',
+      to: email,
+      subject: "Confirme seu email",
+      html: `<div class="container"><h1>Olá, ${username} o código para autenticação é: </h1><br><h2>${code}</h2></div>`,
+    });
+  }
 }
